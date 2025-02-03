890963a16d8b30c21331af7adeb6b40c205d0e74
Gautam Kumar Sharma <81625160+gautam5722@users.noreply.github.com>
2025-02-03 22:46:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-// script.js
-document.addEventListener('DOMContentLoaded', () => {
-    const codes = document.querySelectorAll('.code');
+document.addEventListener("DOMContentLoaded", () => {
+    const inputs = document.querySelectorAll(".code");
 
-    codes[0].focus();
-
-    codes.forEach((code, idx) => {
-        code.addEventListener('input', (e) => {
-            if (e.target.value.length === 1) {
-                if (idx < codes.length - 1) {
-                    codes[idx + 1].focus();
+    inputs.forEach((input, index) => {
+        input.addEventListener("input", (e) => {
+            const value = e.target.value;
+            if (value.match(/[0-9]/)) {
+                if (index < inputs.length - 1) {
+                    inputs[index + 1].focus();
                 }
+            } else {
+                e.target.value = "";
             }
         });
 
-        code.addEventListener('keydown', (e) => {
-            if (e.key === 'Backspace') {
-                if (idx > 0 && e.target.value === '') {
-                    codes[idx - 1].focus();
+        input.addEventListener("keydown", (e) => {
+            if (e.key === "Backspace") {
+                if (input.value === "" && index > 0) {
+                    inputs[index - 1].focus();
                 }
             }
         });
     });
+
+    inputs[0].focus();
 });
